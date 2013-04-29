c2218fff3ab0bc5c287c411cef775ad6f4f07bca
András Veres-Szentkirályi <vsza@vsza.hu>
2013-04-29 18:59:35 +0200

    added OAuthConfig sample

---
+++ OAuthConfig.sample.java
@@ -0,0 +1,19 @@
+package burp;
+
+public class OAuthConfig {
+	public static String getConsumerKey() {
+		return "ConsumerKey";
+	}
+
+	public static String getConsumerSecret() {
+		return "ConsumerSecret";
+	}
+
+	public static String getToken() {
+		return "Token";
+	}
+
+	public static String getTokenSecret() {
+		return "TokenSecret";
+	}
+}
